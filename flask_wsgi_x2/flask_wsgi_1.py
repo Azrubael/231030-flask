3b9ad270580f67044f46f366a28da49fd01055f1
Azrubael <i0638464000@gmail.com>
2023-11-03 13:45:31 +0200

    Step #8. For Linux 'flask_wsgi_x2' were added 'flash messages' about submit success or error.

--- flask_wsgi_x2/flask_wsgi_1.py
+++ flask_wsgi_x2/flask_wsgi_1.py
@@ -1,54 +1,35 @@
-from flask import Flask, render_template, request, url_for, request
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'refrigerRat0r'
 
 menu = [{"name": "Installation", "url": "install-flask"},
         {"name": "First Application", "url": "first-app"},
         {"name": "Feedback", "url": "contact"}]
 
+
 @app.route('/index')
 @app.route('/')
 def index():
-    print( url_for('index') )
     return render_template('index.html', menu=menu)
-    
+
+
 @app.route('/about')
 def about():
-    print( url_for('about') )
     return render_template('about.html', title='The About Page', menu=menu)
+
 
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
-    print( url_for('contact') )
-    if request.method == "POST":
+    if request.method == "POST" or request.method == "GET":
         print(request.form)
         print(f"Message: {request.form['message']}")
+        if len(request.form['username']) > 2:
+            flash('-= Message sent =-', category='success')
+        else:
+            flash('-= Sending error =-', category='error')
+        
     return render_template('contact.html', title='Feedback', menu=menu)
-
-@app.route("/profile/<username>")
-def profile(username):
-    """После ввода в сроке адреса http://127.0.0.1:5000/profile/az
-    выводит надпись 'User: az'."""
-    return f"User: {username}"
-
-@app.route("/useful_app/<path:username>")
-def useful_app(username):
-    """После ввода в сроке адреса http://127.0.0.1:5000/profile/az/longpath
-    выводит надпись 'User: az/longpath'."""
-    return f"User: {username}"
-
-@app.route("/uapp/<int:username>/<path>")
-def uapp(username, path):
-    """После ввода в сроке адреса http://127.0.0.1:5000/profile/324/somebody
-    выводит надпись 'User: 324, somebody'."""
-    return f"User: {username}, {path}"
-
-# Тестовый запрос для проверки работы контекстов без запуска вебсервера
-# with app.test_request_context():
-#     print( url_for('index') )
-#     print( url_for('about') )
-#     print( url_for('profile', username="az") )
-#     print( url_for('useful_app', username="az/longpath") )
 
 
 if __name__ == '__main__':
