301c6c6683173f17594878c21c6b0357067319ea
Azrubael <i0638464000@gmail.com>
2023-11-04 18:41:14 +0200

    Step #9. For Linux 'flask_wsgi_x2' were added 'Page 404' handler, also expanded main menu.

--- flask_wsgi_x2/flask_wsgi_1.py
+++ flask_wsgi_x2/flask_wsgi_1.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'refrigerRat0r'
 
-menu = [{"name": "Installation", "url": "install-flask"},
+menu = [{"name": "Main", "url": "/"},
+        {"name": "Installation", "url": "install-flask"},
         {"name": "First Application", "url": "first-app"},
+        {"name": "About", "url": "about"},
         {"name": "Feedback", "url": "contact"}]
+
 
 
 @app.route('/index')
@@ -21,7 +24,7 @@ def about():
 
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
-    if request.method == "POST" or request.method == "GET":
+    if request.method == "POST":
         print(request.form)
         print(f"Message: {request.form['message']}")
         if len(request.form['username']) > 2:
@@ -30,6 +33,12 @@ def contact():
             flash('-= Sending error =-', category='error')
         
     return render_template('contact.html', title='Feedback', menu=menu)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return  render_template('page404.html', \
+            title='Page not found', menu=menu), 404
 
 
 if __name__ == '__main__':
