1d4d3c67f053c55f4513cbf8040fd2908b5b4c9c
Azrubael <i0638464000@gmail.com>
2023-11-05 19:09:59 +0200

    Step #12. Second commit for Linux 'flask_db' with sqlite3. With posd adding into SQLite3 RDBMS.

---
+++ flask_db/FDataBase.py
@@ -0,0 +1,35 @@
+import math
+import sqlite3
+import time
+
+
+class FDataBase:
+
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+
+    def getMenu(self):
+        sql = """SELECT * FROM mainmenu"""
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except:
+            print("Error reading from DataBase")
+        return []
+    
+
+    def addPost(self, title, text):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)", \
+                               (title, text, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Error adding article to BataBase" + str(e))
+            return False
+        
+        return True
--- flask_db/flsite.py
+++ flask_db/flsite.py
@@ -1,7 +1,8 @@
 import os
 import sqlite3
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, flash
 from decouple import config
+from FDataBase import FDataBase
 
 # DBMS configuation
 DATABASE = config('DATABASE')
@@ -18,7 +19,7 @@ def connect_db():
     """Establishing a connection to the database."""
     conn= sqlite3.connect(app.config['DATABASE'])
 
-    # declaration to ensure that records are returned from the database as dictionaries rather than tuples
+    # to ensure that records are returned from the database as dictionaries rather than tuples
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -42,7 +43,27 @@ def get_db():
 @app.route("/")
 def index():
     db = get_db()
-    return render_template('index.html', menu = [])
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu())
+
+
+@app.route('/add_post', methods=['POST', 'GET'])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post'])  > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Error adding article', category='error')
+            else:
+                flash('Article added successfully', category='success')
+        else:
+            flash('Error adding article', category='error')
+
+    return  render_template('add_post.html', menu=dbase.getMenu(), \
+            title='Adding article')
 
 
 @app.teardown_appcontext
@@ -54,3 +75,5 @@ def close_db(error):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# timecode 09:00
